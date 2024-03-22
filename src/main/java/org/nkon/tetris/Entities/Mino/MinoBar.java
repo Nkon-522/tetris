ea8a6b0699cc013d9a1b4e414f78c999da250b75
Nkon-522 <nkon.cs@gmail.com>
2024-03-22 17:09:25 -0500

    Add - Minos Subtypes

--- src/main/java/org/nkon/tetris/Entities/Mino/MinoBar.java
+++ src/main/java/org/nkon/tetris/Entities/Mino/MinoBar.java
@@ -2,45 +2,45 @@ package org.nkon.tetris.Entities.Mino;
 
 import javafx.scene.paint.Color;
 
-public class MinoZ1 extends Mino{
-    public MinoZ1() {
-        super(Color.RED);
+public class MinoBar extends Mino{
+    public MinoBar() {
+        super(Color.CYAN);
     }
 
     @Override
     public void setXY(int x, int y) {
         b[0].x = x;
         b[0].y = y;
-        b[1].x = b[0].x;
-        b[1].y = b[0].y - Block.SIZE;
-        b[2].x = b[0].x - Block.SIZE;
+        b[1].x = b[0].x - Block.SIZE;
+        b[1].y = b[0].y;
+        b[2].x = b[0].x + Block.SIZE;
         b[2].y = b[0].y;
-        b[3].x = b[0].x - Block.SIZE;
-        b[3].y = b[0].y + Block.SIZE;
+        b[3].x = b[0].x + Block.SIZE * 2;
+        b[3].y = b[0].y;
     }
 
     @Override
     protected void getDirection0() {
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x;
-        tempB[1].y = b[0].y - Block.SIZE;
-        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
         tempB[2].y = b[0].y;
-        tempB[3].x = b[0].x - Block.SIZE;
-        tempB[3].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x + Block.SIZE * 2;
+        tempB[3].y = b[0].y;
     }
 
     @Override
     protected void getDirection1() {
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].x = b[0].x - Block.SIZE;
         tempB[1].y = b[0].y;
-        tempB[2].x = b[0].x;
-        tempB[2].y = b[0].y - Block.SIZE;
-        tempB[3].x = b[0].x - Block.SIZE;
-        tempB[3].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE * 2;
+        tempB[3].y = b[0].y;
     }
 
     @Override
--- src/main/java/org/nkon/tetris/Entities/Mino/MinoSquare.java
+++ src/main/java/org/nkon/tetris/Entities/Mino/MinoSquare.java
@@ -1,4 +1,48 @@
 package org.nkon.tetris.Entities.Mino;
 
-public class MinoSquare {
+import javafx.scene.paint.Color;
+
+public class MinoSquare extends Mino{
+    public MinoSquare() {
+        super(Color.YELLOW);
+    }
+
+    @Override
+    public void setXY(int x, int y) {
+        b[0].x = x;
+        b[0].y = y;
+        b[1].x = b[0].x;
+        b[1].y = b[0].y + Block.SIZE;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
+        b[3].x = b[0].x + Block.SIZE;
+        b[3].y = b[0].y + Block.SIZE;
+    }
+
+    @Override
+    protected void getDirection0() {
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+    }
+
+    @Override
+    protected void getDirection1() {
+        getDirection0();
+    }
+
+    @Override
+    protected void getDirection2() {
+        getDirection0();
+    }
+
+    @Override
+    protected void getDirection3() {
+        getDirection0();
+    }
 }
